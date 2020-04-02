b480ad4c8fb6174c4fd385b028f46845137cc390
Colin Amos <william.amos@utoledo.edu>
2020-04-02 16:23:54 -0400

    progress

--- src/components/slider.js
+++ src/components/slider.js
@@ -7,43 +7,8 @@ import "./css/headshotQuote.css"
 class Slider extends React.Component {
     render() {
         return (
-            <div className="testimonialCarousel">
-                <div className="row">
-						<div className="four columns">
-							<div className="headshotQuote__image"></div>
-						</div>
-						<div className="eight columns">
-							<div className="headshotQuote__text">
-								<h3 className="boldHeading">Experience Toledo&nbsp;Law</h3>
-								<h4 className="script-ignore">Meet <strong>Cameron Morrissey</strong>, 3L</h4>
-								<p>"My time at the U.S. District Court [as a Public Service Fellow] was very hands-on and afforded me the opportunity to see 'behind the curtain' of the practice of law and helped me understand what law looks like on a practical level."</p>
-							</div>
-						</div>
-					</div>
-					<div className="row">
-						<div className="four columns">
-							<div className="headshotQuote__image"></div>
-						</div>
-						<div className="eight columns">
-							<div className="headshotQuote__text">
-								<h3 className="boldHeading">Experience Toledo&nbsp;Law</h3>
-								<h4 className="script-ignore">Meet <strong>Nicole Cote</strong>, 2L</h4>
-								<p>"Whether you are coming directly from an undergraduate institution, taking a couple of years off, or out of school for many years, Toledo Law is an outstanding school that will help you succeed and find your place."</p>
-							</div>
-						</div>
-					</div>
-					<div className="row">
-						<div className="four columns">
-							<div className="headshotQuote__image"></div>
-						</div>
-						<div className="eight columns">
-							<div className="headshotQuote__text">
-								<h3 className="boldHeading">Experience Toledo&nbsp;Law</h3>
-								<h4 className="script-ignore">Meet <strong>Damon Williams</strong>, 2L</h4>
-								<p>"The Toledo way is real and alive in this city. I have been mentored by Toledo Law professors and been given priceless advice and direction. I have also made everlasting friendships with some of the most intelligent and inspirational people I have ever met."</p>
-							</div>
-						</div>
-					</div>
+            <div className="sliderComponent">
+                {this.props.children}
             </div>
         )
     }
@@ -53,7 +18,7 @@ class Slider extends React.Component {
 
             // This function will get called after the slick-carousel.js code
             function activateSlider() {
-                $('.testimonialCarousel').slick({});
+                $('.sliderComponent').slick({});
             }
             // This line is necessary for the below plugin
             window.$ = window.jQuery = require('jquery')
--- src/components/welcome/studentLife.js
+++ src/components/welcome/studentLife.js
@@ -28,7 +28,42 @@ const StudentLifeContent = ({ state }) => {
                     </Row>
                     <Row>
                          <Slider>
-                              
+                              <div className="row">
+                                   <div className="four columns">
+                                        <div className="headshotQuote__image" style={{backgroundImage: "url(https://via.placeholder.com/300x300.png?text=Photo+of+Sally)"}}></div>
+                                   </div>
+                                   <div className="eight columns">
+                                        <div className="headshotQuote__text">
+                                             <h3 className="boldHeading">Experience Toledo&nbsp;Law</h3>
+                                             <h4 className="script-ignore">Meet <strong>Cameron Morrissey</strong>, 3L</h4>
+                                             <p>"My time at the U.S. District Court [as a Public Service Fellow] was very hands-on and afforded me the opportunity to see 'behind the curtain' of the practice of law and helped me understand what law looks like on a practical level."</p>
+                                        </div>
+                                   </div>
+                              </div>
+                              <div className="row">
+                                   <div className="four columns">
+                                   <div className="headshotQuote__image" style={{backgroundImage: "url(https://via.placeholder.com/300x300.png?text=Photo+of+Sally)"}}></div>
+                                   </div>
+                                   <div className="eight columns">
+                                        <div className="headshotQuote__text">
+                                             <h3 className="boldHeading">Experience Toledo&nbsp;Law</h3>
+                                             <h4 className="script-ignore">Meet <strong>Nicole Cote</strong>, 2L</h4>
+                                             <p>"Whether you are coming directly from an undergraduate institution, taking a couple of years off, or out of school for many years, Toledo Law is an outstanding school that will help you succeed and find your place."</p>
+                                        </div>
+                                   </div>
+                              </div>
+                              <div className="row">
+                                   <div className="four columns">
+                                   <div className="headshotQuote__image" style={{backgroundImage: "url(https://via.placeholder.com/300x300.png?text=Photo+of+Sally)"}}></div>
+                                   </div>
+                                   <div className="eight columns">
+                                        <div className="headshotQuote__text">
+                                             <h3 className="boldHeading">Experience Toledo&nbsp;Law</h3>
+                                             <h4 className="script-ignore">Meet <strong>Damon Williams</strong>, 2L</h4>
+                                             <p>"The Toledo way is real and alive in this city. I have been mentored by Toledo Law professors and been given priceless advice and direction. I have also made everlasting friendships with some of the most intelligent and inspirational people I have ever met."</p>
+                                        </div>
+                                   </div>
+                              </div>
                          </Slider>
                     </Row>
                     <br /><br />
